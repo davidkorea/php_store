7b48c97005e841febd310234bd86e4b92b2c051d
davidkorea <lyfirst@naver.com>
2018-06-09 00:25:17 +0900

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>php store</title>
+</head>
+<body>
+hello,
+<?php
+echo "this is from php";
+?>
+
+
+</body>
+</html>
