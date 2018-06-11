a44efd38deb2f0ea79d886b15fb2ea1fc61ec191
davidkorea <lyfirst@naver.com>
2018-06-12 00:29:48 +0900

    category edit page

--- category_edit.php
+++ category_edit.php
@@ -1,4 +1,34 @@
-<?php require_once 'mysql.php'; ?>
+<?php
+require_once 'mysql.php';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_POST['id'] > 0) {
+        $sql = "update category set `code`=?,`name`=? where id=?";
+        $stmt = mysqli_prepare($mysqli, $sql);
+        mysqli_stmt_bind_param($stmt, 'ssi', $_POST['code'], $_POST['name'], $_POST['id']);
+    } else {
+        $sql = "insert into category (`code`,`name`) VALUES (?,?)";
+        $stmt = mysqli_prepare($mysqli, $sql);
+        mysqli_stmt_bind_param($stmt, 'ss', $_POST['code'], $_POST['name']);
+    }
+    $result = mysqli_stmt_execute($stmt);
+    if ($result) {
+        header("Location: category.php");
+        exit;
+    } else {
+        exit('商品分类新增/修改失败');
+    }
+}
+
+$sql = "select * from category where id=?";
+$stmt = mysqli_prepare($mysqli, $sql);
+mysqli_stmt_bind_param($stmt, 'i', $_GET['id']);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+$row = mysqli_fetch_assoc($result);
+$id = $row['id'];
+$code = $row['code'];
+$name = $row['name'];
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -17,6 +47,22 @@
         <?php require_once 'sidebar.php' ?>
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
             <h6>商品分类-编辑</h6>
+            <form action="category_edit.php" method="post">
+                <div class="form-group row">
+                    <label for="id" class="col-sm-2 col-form-label">序号</label>
+                    <input type="text" class="form-control col-sm-6" id="id" name="id" value="<?php echo $id ?>"
+                           readonly>
+                </div>
+                <div class="form-group row">
+                    <label for="code" class="col-sm-2 col-form-label">编码</label>
+                    <input type="text" class="form-control col-sm-6" id="code" name="code" value="<?php echo $code ?>">
+                </div>
+                <div class="form-group row">
+                    <label for="name" class="col-sm-2 col-form-label">分类名称</label>
+                    <input type="text" class="form-control col-sm-6" id="name" name="name" value="<?php echo $name ?>">
+                </div>
+                <button type="submit" class="btn btn-primary">修改</button>
+            </form>
         </main>
     </div>
 </div>
