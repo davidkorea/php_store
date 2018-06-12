1acc90a6bfa9b3d95909975d157c776a53aa15cd
davidkorea <lyfirst@naver.com>
2018-06-12 22:15:00 +0900

    jquery full file link

--- footer.php
+++ footer.php
@@ -2,7 +2,8 @@
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+<!-- slim为简化版，min以为取消换行空格来压缩文件以便网页快速加载-->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
